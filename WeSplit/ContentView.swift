71ba97e49f5f3caa5a1dce4e30d8eadd685dff61
Faisal Babkoor <f.babkoor.tera@gmail.com>
2020-02-18 10:10:08 +0300

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Faisal Babkoor on 18/02/2020.
+//  Copyright © 2020 Faisal Babkoor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
